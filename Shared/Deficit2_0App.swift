705c73293d2a2bab9e7eb6cd02f7131306e140f3
Yotzin Castrejon <yotzincastrejon@yahoo.com>
2021-10-18 11:55:49 -0700

    Initial Commit

---
+++ Shared/Deficit2_0App.swift
@@ -0,0 +1,17 @@
+//
+//  Deficit2_0App.swift
+//  Shared
+//
+//  Created by Yotzin Castrejon on 10/18/21.
+//
+
+import SwiftUI
+
+@main
+struct Deficit2_0App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
